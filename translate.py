51de4d99695a96ab09b0d5e583e45065a506da71
home_computer <dalekx350@gmail.com>
2020-05-13 14:37:38 +1000

    kupi slona

--- translate.py
+++ translate.py
@@ -2,4 +2,4 @@ import requests
 
 
 def translate_text(string):
-    return requests.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20200513T040902Z.ef11949d385c6eee.aa537b0a8e95ca3c5cf09f86bb31f597c65be0d1&text='+ string +'&lang=ru-en').json()['text'][0]
+    return requests.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20200513T040902Z.ef11949d385c6eee.aa537b0a8e95ca3c5cf09f86bb31f597c65be0d1&text='+ string +'&lang=ru-en').json()
