d6d78f2cc3afa07ffb12e2b10553fdafde8326df
home_computer <dalekx350@gmail.com>
2020-05-13 15:12:31 +1000

    kupi slona

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from flask import Flask, request
-from translate import translate_text
 import logging
 import json
 import os
 
+from geo import get_country, get_distance, get_coordinates
+
 app = Flask(__name__)
-logging.basicConfig(level=logging.INFO)
-sessionStorage = {}
+
+
+logging.basicConfig(level=logging.INFO, filename='app.log',
+                    format='%(asctime)s %(levelname)s %(name)s %(message)s')
 
 
 @app.route('/post', methods=['POST'])
 def main():
-    logging.info(f'Request: {request.json!r}')
+    logging.info('Request: %r', request.json)
     response = {
         'session': request.json['session'],
         'version': request.json['version'],
@@ -20,24 +23,39 @@ def main():
         }
     }
     handle_dialog(response, request.json)
-    logging.info(f'Response: {response!r}')
+    logging.info('Request: %r', response)
     return json.dumps(response)
 
 
 def handle_dialog(res, req):
     user_id = req['session']['user_id']
     if req['session']['new']:
-        res['response']['text'] = 'Привет, какое слово перевести?'
-        sessionStorage[user_id] = {
-            'first_name': None
-        }
+        res['response']['text'] = \
+            'Привет! Я могу показать город или сказать расстояние между городами!'
         return
-    if 'переведи слово' == req['request']['original_utterance'].lower()[:14]:
-        res['response']['text'] = translate_text(req['request']['original_utterance'][14:])
-    elif 'переведите слово' == req['request']['original_utterance'].lower()[:16]:
-        res['response']['text'] = translate_text(req['request']['original_utterance'][16:])
+
+    cities = get_cities(req)
+    if not cities:
+        res['response']['text'] = 'Ты не написал название не одного города!'
+    elif len(cities) == 1:
+        res['response']['text'] = 'Этот город в стране - ' + \
+                                  get_country(cities[0])
+    elif len(cities) == 2:
+        distance = get_distance(get_coordinates(
+            cities[0]), get_coordinates(cities[1]))
+        res['response']['text'] = 'Расстояние между этими городами: ' + \
+                                  str(round(distance)) + ' км.'
     else:
-        res['response']['text'] = 'Не совсем понимаю что вы хотите перевести'
+        res['response']['text'] = 'Слишком много городов!'
+
+
+def get_cities(req):
+    cities = []
+    for entity in req['request']['nlu']['entities']:
+        if entity['type'] == 'YANDEX.GEO':
+            if 'city' in entity['value']:
+                cities.append(entity['value']['city'])
+    return cities
 
 
 if __name__ == '__main__':
