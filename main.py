9771114577f89d31977279c0cf4645bbfd4aab99
home_computer <dalekx350@gmail.com>
2020-05-13 15:38:00 +1000

    aliceandmaps

--- main.py
+++ main.py
@@ -38,11 +38,13 @@ def handle_dialog(res, req):
     if not cities:
         res['response']['text'] = 'Ты не написал название не одного города!'
     elif len(cities) == 1:
-        res['response']['text'] = 'Этот город в стране - '
+        res['response']['text'] = 'Этот город в стране - ' + \
+                                  get_country(cities[0])
     elif len(cities) == 2:
-
+        distance = get_distance(get_coordinates(
+            cities[0]), get_coordinates(cities[1]))
         res['response']['text'] = 'Расстояние между этими городами: ' + \
-' км.'
+                                  str(round(distance)) + ' км.'
     else:
         res['response']['text'] = 'Слишком много городов!'
 
