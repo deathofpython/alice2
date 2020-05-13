5d7fbf06c438d48eb6b346fe5435672439e4e68a
home_computer <dalekx350@gmail.com>
2020-05-13 14:24:05 +1000

    kupi slona

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
 from flask import Flask, request
+from translate import translate_text
 import logging
 import json
-import random
 import os
 
 app = Flask(__name__)
-
 logging.basicConfig(level=logging.INFO)
-
-cities = {
-    'москва': ['1030494/d65c027ddf6999410da1',
-               '1533899/4addb94582d6aeebcd72'],
-    'нью-йорк': ['1030494/53ad51bd7755ec741585',
-                 '1652229/033f69506e617d2f76c1'],
-    'париж': ["1656841/f1ae19da60f784606c2c",
-              '1030494/94558b76f041b926cb68'],
-}
-
 sessionStorage = {}
 
 
@@ -37,115 +26,18 @@ def main():
 
 def handle_dialog(res, req):
     user_id = req['session']['user_id']
-
-    # если пользователь новый, то просим его представиться.
     if req['session']['new']:
-        res['response']['text'] = 'Привет! Назови свое имя!'
-        # создаем словарь в который в будущем положим имя пользователя
+        res['response']['text'] = 'Привет, какое слово перевести?'
         sessionStorage[user_id] = {
             'first_name': None
         }
         return
 
-    # если пользователь не новый, то попадаем сюда.
-    # если поле имени пустое, то это говорит о том,
-    # что пользователь еще не представился.
-    if sessionStorage[user_id]['first_name'] is None:
-        # в последнем его сообщение ищем имя.
-        first_name = get_first_name(req)
-        # если не нашли, то сообщаем пользователю что не расслышали.
-        if first_name is None:
-            res['response']['text'] = \
-                'Не расслышала имя. Повтори, пожалуйста!'
-        # если нашли, то приветствуем пользователя.
-        # И спрашиваем какой город он хочет увидеть.
-        else:
-            sessionStorage[user_id]['first_name'] = first_name
-            res['response'][
-                'text'] = 'Приятно познакомиться, ' \
-                          + first_name.title() \
-                          + '. Я - Алиса. Какой город хочешь увидеть?'
-            # получаем варианты buttons из ключей нашего словаря cities
-            res['response']['buttons'] = [
-                {
-                    'title': 'Помощь',
-                    'hide': True
-                },
-                {
-                    'title': 'Москва',
-                    'hide': True
-                },
-                {
-                    'title': 'Париж',
-                    'hide': True
-                },
-                {
-                    'title': 'Нью-Йорк',
-                    'hide': True
-                }
-            ]
-
-    # если мы знакомы с пользователем и он нам что-то написал,
-    # то это говорит о том, что он уже говорит о городе,
-    # что хочет увидеть.
+    if 'переведи слово' in req.lower():
+        res['response']['text'] = translate_text(req[14:])
     else:
-        # ищем город в сообщение от пользователя
-        city = get_city(req)
-        # если этот город среди известных нам,
-        # то показываем его (выбираем одну из двух картинок случайно)
-        if city in cities:
-            res['response']['card'] = {}
-            res['response']['card']['type'] = 'BigImage'
-            res['response']['card']['title'] = 'Этот город я знаю.'
-            res['response']['card']['image_id'] = random.choice(cities[city])
-            res['response']['text'] = 'Я угадал!'
-        # если не нашел, то отвечает пользователю
-        # 'Первый раз слышу об этом городе.'
-        else:
-            if 'Помощь' in req['request']['original_utterance']:
-                res['response']['text'] = 'Справка по игре: доступные города - Москва, Париж, Нью-Йорк'
-            else:
-                res['response']['text'] = \
-                    'Первый раз слышу об этом городе. Попробуй еще разок!'
-        res['response']['buttons'] = [
-            {
-                'title': 'Помощь',
-                'hide': True
-            },
-            {
-                'title': 'Москва',
-                'hide': True
-            },
-            {
-                'title': 'Париж',
-                'hide': True
-            },
-            {
-                'title': 'Нью-Йорк',
-                'hide': True
-            }
-        ]
-
-
-def get_city(req):
-    # перебираем именованные сущности
-    for entity in req['request']['nlu']['entities']:
-        # если тип YANDEX.GEO то пытаемся получить город(city),
-        # если нет, то возвращаем None
-        if entity['type'] == 'YANDEX.GEO':
-            # возвращаем None, если не нашли сущности с типом YANDEX.GEO
-            return entity['value'].get('city', None)
-
-
-def get_first_name(req):
-    # перебираем сущности
-    for entity in req['request']['nlu']['entities']:
-        # находим сущность с типом 'YANDEX.FIO'
-        if entity['type'] == 'YANDEX.FIO':
-            # Если есть сущность с ключом 'first_name',
-            # то возвращаем ее значение.
-            # Во всех остальных случаях возвращаем None.
-            return entity['value'].get('first_name', None)
+        res['response']['text'] = \
+            'Чтобы получить перевод слова введите команду \"переведи слово\" + (\'text\')'
 
 
 if __name__ == '__main__':
